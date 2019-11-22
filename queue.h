0cf22cb5a96c265e76060d1c50b0fac5085979d0
Jiahong Long <jiahong888long@gmail.com>
2019-11-22 12:34:00 -0800

    Initial implementation of the Task class and skeleton for the HashQueue of Tasks.

---
+++ queue.h
@@ -0,0 +1,10 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "task.h"
+#include <vector>
+
+class HashQueue {
+};
+
+#endif // QUEUE_H
---
+++ task.h
@@ -0,0 +1,45 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <chrono>
+
+using namespace std::chrono;
+
+class Task {
+    private:
+        int priority_;
+        minutes time_mins_;
+        int Clamp(int priority);
+    public:
+        static const int kMaxPriority = 5000;
+        static const int kMinPriority = 0;
+
+        Task(const Task &task);
+        explicit Task(int priority);
+        Task(int priority, minutes time_mins);
+        Task(int priority, int time_mins);
+};
+
+// Clamps priority to static constants
+int Task::Clamp(int priority) {
+    if (priority > kMaxPriority) return kMaxPriority;
+    else if (priority < kMinPriority) return kMinPriority;
+    else return priority;
+}
+
+// Copy constructor
+Task::Task(const Task &task) : priority_(task.priority_) {}
+
+Task::Task(int priority) :
+    priority_(Clamp(priority)),
+    time_mins_(minutes(0)) {}
+
+Task::Task(int priority, minutes time_mins) :
+    priority_(Clamp(priority)),
+    time_mins_(minutes(time_mins)) {}
+
+Task::Task(int priority, int time_mins) :
+    priority_(Clamp(priority)),
+    time_mins_(minutes(time_mins)) {}
+
+#endif // TASK_H
